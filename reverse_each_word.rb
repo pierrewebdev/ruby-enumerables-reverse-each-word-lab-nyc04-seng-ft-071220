4aaed33958c70bf26d3080e21b2455d379584d59
Patrick Pierre <patrick.pierre000@gmail.com>
2020-07-29 03:54:00 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(str)
     #separate the string into an array
-    reversed_string_array = str.split.map {|word|word.reverse}
+    reversed_string_array = str.split.collectlea {|word|word.reverse}
     #implicitly return my rversed joined string
     puts reversed_string_array.join(" ")
     reversed_string_array.join(" ")
